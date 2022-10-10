3263355a0cb0a5304b113d664ba49fad287a4db8
Tanmoy Sarkar <ts741127@gmail.com>
2022-10-10 22:21:35 +0530

    Flutter version upgraded and issues fixed with widget test

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,25 @@ import 'package:notes_era/login.dart';
 import 'package:notes_era/listnotes.dart';
 import 'package:notes_era/expandnote.dart';
 
-void run() => {
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/',
-    routes: {
-      '/': (context) => Splash(),
-      '/welcome': (context) => Welcome(),
-      '/signup': (context) => Signup(),
-      '/login': (context) => Login(),
-      '/list': (context) => ListNotes(),
-      '/expand': (context) => ExpandNotes()
-    }
-  )
-};
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Splash(),
+          '/welcome': (context) => Welcome(),
+          '/signup': (context) => Signup(),
+          '/login': (context) => Login(),
+          '/list': (context) => ListNotes(),
+          '/expand': (context) => ExpandNotes()
+        });
+  }
+}
