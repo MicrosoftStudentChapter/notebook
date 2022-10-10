faa92fb0635f402da25ab6fcf39a891c700fa685
Tanmoy Sarkar <ts741127@gmail.com>
2022-10-10 23:02:00 +0530

    Splash screen completed for both theme [light and dark]

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:notes_era/spalsh.dart';
 import 'package:notes_era/welcome.dart';
 import 'package:notes_era/signup.dart';
@@ -16,15 +17,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        routes: {
-          '/': (context) => Splash(),
-          '/welcome': (context) => Welcome(),
-          '/signup': (context) => Signup(),
-          '/login': (context) => Login(),
-          '/list': (context) => ListNotes(),
-          '/expand': (context) => ExpandNotes()
-        });
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      themeMode: ThemeMode.system,
+      routes: {
+        '/': (context) => Splash(),
+        '/welcome': (context) => Welcome(),
+        '/signup': (context) => Signup(),
+        '/login': (context) => Login(),
+        '/list': (context) => ListNotes(),
+        '/expand': (context) => ExpandNotes()
+      },
+      //  Light theme details
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
+        primaryColorDark: Colors.black,
+        canvasColor: Colors.white,
+        appBarTheme: const AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle(statusBarBrightness: Brightness.light)),
+      ),
+      // Dark theme details
+      darkTheme: ThemeData(
+        primaryColor: Colors.black,
+        primaryColorLight: Colors.black,
+        brightness: Brightness.dark,
+        primaryColorDark: Colors.black,      
+        indicatorColor: Colors.white,
+        canvasColor: Colors.black,
+        appBarTheme: const AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle(statusBarBrightness: Brightness.dark)),
+      ),
+    );
   }
 }
